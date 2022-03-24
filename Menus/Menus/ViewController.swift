c49fea84fc5881b86c30aa1399d249e4ac8496f9
katherined23 <71879526+katherined23@users.noreply.github.com>
2022-03-24 11:34:29 +0000

    Add files via upload

---
+++ Menus/Menus/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Menus
+//
+//  Created by katherine demetris on 23/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBAction func SchedulePressed(_ sender: Any) {
+    }
+    @IBAction func AttendencePressed(_ sender: Any) {
+    }
+    @IBAction func GradesPressed(_ sender: Any) {
+    }
+    @IBOutlet weak var gradesPressed: UIButton!
+        
+    @IBAction func HomePresed(_ sender: Any) {
+    }
+    @IBAction func Home2Pressed(_ sender: Any) {
+    }
+    
+    @IBAction func Home3Pressed(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
