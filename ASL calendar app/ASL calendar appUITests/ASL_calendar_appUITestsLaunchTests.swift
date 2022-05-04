379f6c949f616fc5675ca12577253a7edc8d0204
katherined23 <71879526+katherined23@users.noreply.github.com>
2022-05-04 12:05:48 +0100

    Add files via upload

---
+++ ASL calendar app/ASL calendar appUITests/ASL_calendar_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ASL_calendar_appUITestsLaunchTests.swift
+//  ASL calendar appUITests
+//
+//  Created by Katherine Demetris on 4/30/22.
+//
+
+import XCTest
+
+class ASL_calendar_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
